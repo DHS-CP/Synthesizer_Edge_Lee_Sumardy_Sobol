cad9310526d3ef1f98c311cf34c441baa8651b81
arinasoboll <31044419+arinasoboll@users.noreply.github.com>
2017-11-02 10:37:05 -0700

    Update SynthesizerActivity.java

--- Synthesizer/app/src/main/java/com/example/lee55140/synthesizer/SynthesizerActivity.java
+++ Synthesizer/app/src/main/java/com/example/lee55140/synthesizer/SynthesizerActivity.java
@@ -13,285 +13,306 @@ import android.widget.Button;
 import static android.content.ContentValues.TAG;
 
 public class SynthesizerActivity extends AppCompatActivity implements MediaPlayer.OnCompletionListener {
-   private static final String TAG = SynthesizerActivity.class.getName();
-   private Button button1;
-   private Button button2;
-   private Button button3;
-   private Button button4;
-   private Button button5;
-   private Button button6;
-   private Button button7;
-   private Button button8;
-   private Button button9;
-   private Button button10;
-   private Button button11;
-   private Button button12;
-   private Button button13;
-   private Button button14;
-   private Button button15;
-   private Button button16;
-   private Button button17;
-   private MediaPlayer mpE;
-   private MediaPlayer mpF;
-   private MediaPlayer mpA;
-   private MediaPlayer mpB;
-   private MediaPlayer mpBB;
-   private MediaPlayer mpC;
-   private MediaPlayer mpCS;
-   private MediaPlayer mpD;
-   private MediaPlayer mpDS;
-   private MediaPlayer mpFS;
-   private MediaPlayer mpG;
-   private MediaPlayer mpGS;
-   private MediaPlayer mpHighE;
-   private MediaPlayer mpHighF;
-   private MediaPlayer mpHighFS;
-   private MediaPlayer mpHighG;
-   private MediaPlayer mChallenge5;
+    private static final String TAG = SynthesizerActivity.class.getName();
+    private Button button1;
+    private Button button2;
+    private Button button3;
+    private Button button4;
+    private Button button5;
+    private Button button6;
+    private Button button7;
+    private Button button8;
+    private Button button9;
+    private Button button10;
+    private Button button11;
+    private Button button12;
+    private Button button13;
+    private Button button14;
+    private Button button15;
+    private Button button16;
+    private Button button17;
+    private MediaPlayer mpE;
+    private MediaPlayer mpF;
+    private MediaPlayer mpA;
+    private MediaPlayer mpB;
+    private MediaPlayer mpBB;
+    private MediaPlayer mpC;
+    private MediaPlayer mpCS;
+    private MediaPlayer mpD;
+    private MediaPlayer mpDS;
+    private MediaPlayer mpFS;
+    private MediaPlayer mpG;
+    private MediaPlayer mpGS;
+    private MediaPlayer mpHighE;
+    private MediaPlayer mpHighF;
+    private MediaPlayer mpHighFS;
+    private MediaPlayer mpHighG;
+    private MediaPlayer mChallenge5;
+    private Button button19;
 
 
-   private final int WHOLE_NOTE = 1000;
+    private final int WHOLE_NOTE = 1000;
 
-   @Override
-   protected void onCreate(Bundle savedInstanceState) {
-       super.onCreate(savedInstanceState);
-       setContentView(R.layout.activity_synthesizer);
-
-
-
-       button1 = (Button) findViewById(R.id.eButton);
-       button2 = (Button) findViewById(R.id.fButton);
-       button3 = (Button) findViewById(R.id.aButton);
-       button4 = (Button) findViewById(R.id.bButton);
-       button5 = (Button) findViewById(R.id.bbButton);
-       button6 = (Button) findViewById(R.id.cButton);
-       button7 = (Button) findViewById(R.id.csButton);
-       button8 = (Button) findViewById(R.id.dButton);
-       button9 = (Button) findViewById(R.id.dsButton);
-       button10 = (Button) findViewById(R.id.fsButton);
-       button11 = (Button) findViewById(R.id.gButton);
-       button12 = (Button) findViewById(R.id.gsButton);
-       button13 = (Button) findViewById(R.id.higheButton);
-       button14 = (Button) findViewById(R.id.highfButton);
-       button15 = (Button) findViewById(R.id.highfsButton);
-       button16 = (Button) findViewById(R.id.highgButton);
-       button17 = (Button) findViewById(R.id.mChallenge5);
-
-       mpE = MediaPlayer.create(this, R.raw.scalee);
-       mpF = MediaPlayer.create(this, R.raw.scalef);
-       mpA = MediaPlayer.create(this, R.raw.scalea);
-       mpB = MediaPlayer.create(this, R.raw.scaleb);
-       mpBB = MediaPlayer.create(this, R.raw.scalebb);
-       mpC = MediaPlayer.create(this, R.raw.scalec);
-       mpCS = MediaPlayer.create(this, R.raw.scalecs);
-       mpD = MediaPlayer.create(this, R.raw.scaled);
-       mpDS = MediaPlayer.create(this, R.raw.scaleds);
-       mpFS = MediaPlayer.create(this, R.raw.scalefs);
-       mpG = MediaPlayer.create(this, R.raw.scaleg);
-       mpGS = MediaPlayer.create(this, R.raw.scalegs);
-       mpHighE = MediaPlayer.create(this, R.raw.scalehighe);
-       mpHighF = MediaPlayer.create(this, R.raw.scalehighf);
-       mpHighFS = MediaPlayer.create(this, R.raw.scalehighfs);
-       mpHighG = MediaPlayer.create(this, R.raw.scalehighg);
-
-       button17.setOnClickListener(new View.OnClickListener() {
-           @Override
-           public void onClick(View v) {
-               Log.e("SynthesizerActivity", "Challenge Twinkle Twinkle Little Star");
-               try {
-                   mpA.start();
-                   delayPlaying(WHOLE_NOTE/2);
-                   mpA.pause();
-                   delayPlaying(WHOLE_NOTE/2);
-                   mpA.start();
-
-                   delayPlaying(WHOLE_NOTE/2);
-                   mpHighE.start();
-                   delayPlaying(WHOLE_NOTE/2);
-                   mpHighE.pause();
-                   delayPlaying(WHOLE_NOTE/2);
-                   mpHighE.start();
-                   delayPlaying(WHOLE_NOTE/2);
-                   mpHighE.pause();
-
-                   delayPlaying(WHOLE_NOTE/2);
-                   mpHighFS.start();
-                   delayPlaying(WHOLE_NOTE/2);
-                   mpHighFS.pause();
-                   delayPlaying(WHOLE_NOTE/2);
-                   mpHighFS.start();
-
-                   delayPlaying(WHOLE_NOTE);
-                   mpHighE.start();
-                   delayPlaying(WHOLE_NOTE/2);
-                   mpHighE.pause();
-
-                   delayPlaying(WHOLE_NOTE/2);
-                   mpD.start();
-                   delayPlaying(WHOLE_NOTE/2);
-                   mpD.pause();
-                   delayPlaying(WHOLE_NOTE/2);
-                   mpD.start();
-
-                   delayPlaying(WHOLE_NOTE/2);
-                   mpCS.start();
-                   delayPlaying(WHOLE_NOTE/2);
-                   mpCS.pause();
-                   delayPlaying(WHOLE_NOTE/2);
-                   mpCS.start();
-
-                   delayPlaying(WHOLE_NOTE/2);
-                   mpB.start();
-                   delayPlaying(WHOLE_NOTE/2);
-                   mpB.pause();
-                   delayPlaying(WHOLE_NOTE/2);
-                   mpB.start();
-
-                   delayPlaying(WHOLE_NOTE/2);
-                   mpA.start();
-                   delayPlaying(WHOLE_NOTE/2);
-                   mpA.pause();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_synthesizer);
 
 
-               } catch (InterruptedException e) {
-                   e.printStackTrace();
-               }
-           }
-       });
 
-   }
+        button1 = (Button) findViewById(R.id.eButton);
+        button2 = (Button) findViewById(R.id.fButton);
+        button3 = (Button) findViewById(R.id.aButton);
+        button4 = (Button) findViewById(R.id.bButton);
+        button5 = (Button) findViewById(R.id.bbButton);
+        button6 = (Button) findViewById(R.id.cButton);
+        button7 = (Button) findViewById(R.id.csButton);
+        button8 = (Button) findViewById(R.id.dButton);
+        button9 = (Button) findViewById(R.id.dsButton);
+        button10 = (Button) findViewById(R.id.fsButton);
+        button11 = (Button) findViewById(R.id.gButton);
+        button12 = (Button) findViewById(R.id.gsButton);
+        button13 = (Button) findViewById(R.id.higheButton);
+        button14 = (Button) findViewById(R.id.highfButton);
+        button15 = (Button) findViewById(R.id.highfsButton);
+        button16 = (Button) findViewById(R.id.highgButton);
+        button17 = (Button) findViewById(R.id.twinkle);
 
+        mpE = MediaPlayer.create(this, R.raw.scalee);
+        mpF = MediaPlayer.create(this, R.raw.scalef);
+        mpA = MediaPlayer.create(this, R.raw.scalea);
+        mpB = MediaPlayer.create(this, R.raw.scaleb);
+        mpBB = MediaPlayer.create(this, R.raw.scalebb);
+        mpC = MediaPlayer.create(this, R.raw.scalec);
+        mpCS = MediaPlayer.create(this, R.raw.scalecs);
+        mpD = MediaPlayer.create(this, R.raw.scaled);
+        mpDS = MediaPlayer.create(this, R.raw.scaleds);
+        mpFS = MediaPlayer.create(this, R.raw.scalefs);
+        mpG = MediaPlayer.create(this, R.raw.scaleg);
+        mpGS = MediaPlayer.create(this, R.raw.scalegs);
+        mpHighE = MediaPlayer.create(this, R.raw.scalehighe);
+        mpHighF = MediaPlayer.create(this, R.raw.scalehighf);
+        mpHighFS = MediaPlayer.create(this, R.raw.scalehighfs);
+        mpHighG = MediaPlayer.create(this, R.raw.scalehighg);
 
-   private void delayPlaying(int delay) throws InterruptedException {
-       try {
-           Thread.sleep(delay);
-       } catch (InterruptedException e) {
-           Log.e("SynthesizerActivity", "Audio playback interrupted");
-       }
-   }
+        button17.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.e("SynthesizerActivity", "Challenge Twinkle Twinkle Little Star");
+                try {
+                    mpA.start();
+                    delayPlaying(WHOLE_NOTE/2);
+                    mpA.pause();
+                    delayPlaying(WHOLE_NOTE/2);
+                    mpA.start();
 
+                    delayPlaying(WHOLE_NOTE/2);
+                    mpHighE.start();
+                    delayPlaying(WHOLE_NOTE/2);
+                    mpHighE.pause();
+                    delayPlaying(WHOLE_NOTE/2);
+                    mpHighE.start();
+                    delayPlaying(WHOLE_NOTE/2);
+                    mpHighE.pause();
 
-   public void onButton1Click(View v) {
-       mpE.seekTo(0);
-       Log.i(TAG, "Button 1 Clicked");
-       mpE.start();
+                    delayPlaying(WHOLE_NOTE/2);
+                    mpHighFS.start();
+                    delayPlaying(WHOLE_NOTE/2);
+                    mpHighFS.pause();
+                    delayPlaying(WHOLE_NOTE/2);
+                    mpHighFS.start();
 
-   }
+                    delayPlaying(WHOLE_NOTE);
+                    mpHighE.start();
+                    delayPlaying(WHOLE_NOTE/2);
+                    mpHighE.pause();
 
-   public void onButton2Click(View v) {
-       mpF.seekTo(0);
-       Log.i(TAG, "Button 2 Clicked");
-       mpF.start();
+                    delayPlaying(WHOLE_NOTE/2);
+                    mpD.start();
+                    delayPlaying(WHOLE_NOTE/2);
+                    mpD.pause();
+                    delayPlaying(WHOLE_NOTE/2);
+                    mpD.start();
 
-   }
+                    delayPlaying(WHOLE_NOTE/2);
+                    mpCS.start();
+                    delayPlaying(WHOLE_NOTE/2);
+                    mpCS.pause();
+                    delayPlaying(WHOLE_NOTE/2);
+                    mpCS.start();
 
-   public void onButton3Click(View v) {
-       mpA.seekTo(0);
-       Log.i(TAG, "Button 3 Clicked");
-       mpA.start();
+                    delayPlaying(WHOLE_NOTE/2);
+                    mpB.start();
+                    delayPlaying(WHOLE_NOTE/2);
+                    mpB.pause();
+                    delayPlaying(WHOLE_NOTE/2);
+                    mpB.start();
 
-   }
-
-   public void onButton4Click(View v) {
-       mpB.seekTo(0);
-       Log.i(TAG, "Button 4 Clicked");
-       mpB.start();
-
-   }
-
-   public void onButton5Click(View v) {
-       mpBB.seekTo(0);
-       Log.i(TAG, "Button 5 Clicked");
-       mpBB.start();
-
-   }
-
-   public void onButton6Click(View v) {
-       mpC.seekTo(0);
-       Log.i(TAG, "Button 6 Clicked");
-       mpC.start();
-
-   }
-
-   public void onButton7Click(View v) {
-       mpCS.seekTo(0);
-       Log.i(TAG, "Button 7 Clicked");
-       mpCS.start();
-
-   }
-
-   public void onButton8Click(View v) {
-       mpD.seekTo(0);
-       Log.i(TAG, "Button 8 Clicked");
-       mpD.start();
-
-   }
-
-   public void onButton9Click(View v) {
-       mpDS.seekTo(0);
-       Log.i(TAG, "Button 9 Clicked");
-       mpDS.start();
-
-   }
-
-   public void onButton10Click(View v) {
-       mpFS.seekTo(0);
-       Log.i(TAG, "Button 10 Clicked");
-       mpFS.start();
-
-   }
-
-   public void onButton11Click(View v) {
-       mpG.seekTo(0);
-       Log.i(TAG, "Button 11 Clicked");
-       mpG.start();
-
-   }
-
-   public void onButton12Click(View v) {
-       mpGS.seekTo(0);
-       Log.i(TAG, "Button 12 Clicked");
-       mpGS.start();
-
-   }
-
-   public void onButton13Click(View v) {
-       mpHighE.seekTo(0);
-       Log.i(TAG, "Button 13 Clicked");
-       mpHighE.start();
-
-   }
-
-   public void onButton14Click(View v) {
-       mpHighF.seekTo(0);
-       Log.i(TAG, "Button 14 Clicked");
-       mpHighF.start();
-
-   }
-
-   public void onButton15Click(View v) {
-       mpHighFS.seekTo(0);
-       Log.i(TAG, "Button 15 Clicked");
-       mpHighFS.start();
-
-   }
-
-   public void onButton16Click(View v) {
-       mpHighG.seekTo(0);
-       Log.i(TAG, "Button 16 Clicked");
-       mpHighG.start();
-
-   }
+                    delayPlaying(WHOLE_NOTE/2);
+                    mpA.start();
+                    delayPlaying(WHOLE_NOTE/2);
+                    mpA.pause();
 
 
-   @Override
-   public void onCompletion(MediaPlayer mp) {
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        button19.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.e("SynthesizerActivity", "Challenge 0 Button clicked");
+                try {
+                    mpE.start();
+                    delayPlaying(WHOLE_NOTE);
+                    mpFS.start();
+                    delayPlaying(WHOLE_NOTE);
+                    mpG.start();
+                    mpA.start();
+                    delayPlaying(WHOLE_NOTE);
+                    mpB.start();
+                    delayPlaying(WHOLE_NOTE);
+                    mpCS.start();
+                    mpD.start();
+                    delayPlaying(WHOLE_NOTE);
+                    mpE.start();
+                    delayPlaying(WHOLE_NOTE);
 
-   }
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+
+
+    private void delayPlaying(int delay) throws InterruptedException {
+        try {
+            Thread.sleep(delay);
+        } catch (InterruptedException e) {
+            Log.e("SynthesizerActivity", "Audio playback interrupted");
+        }
+    }
+
+
+    public void onButton1Click(View v) {
+        mpE.seekTo(0);
+        Log.i(TAG, "Button 1 Clicked");
+        mpE.start();
+
+    }
+
+    public void onButton2Click(View v) {
+        mpF.seekTo(0);
+        Log.i(TAG, "Button 2 Clicked");
+        mpF.start();
+
+    }
+
+    public void onButton3Click(View v) {
+        mpA.seekTo(0);
+        Log.i(TAG, "Button 3 Clicked");
+        mpA.start();
+
+    }
+
+    public void onButton4Click(View v) {
+        mpB.seekTo(0);
+        Log.i(TAG, "Button 4 Clicked");
+        mpB.start();
+
+    }
+
+    public void onButton5Click(View v) {
+        mpBB.seekTo(0);
+        Log.i(TAG, "Button 5 Clicked");
+        mpBB.start();
+
+    }
+
+    public void onButton6Click(View v) {
+        mpC.seekTo(0);
+        Log.i(TAG, "Button 6 Clicked");
+        mpC.start();
+
+    }
+
+    public void onButton7Click(View v) {
+        mpCS.seekTo(0);
+        Log.i(TAG, "Button 7 Clicked");
+        mpCS.start();
+
+    }
+
+    public void onButton8Click(View v) {
+        mpD.seekTo(0);
+        Log.i(TAG, "Button 8 Clicked");
+        mpD.start();
+
+    }
+
+    public void onButton9Click(View v) {
+        mpDS.seekTo(0);
+        Log.i(TAG, "Button 9 Clicked");
+        mpDS.start();
+
+    }
+
+    public void onButton10Click(View v) {
+        mpFS.seekTo(0);
+        Log.i(TAG, "Button 10 Clicked");
+        mpFS.start();
+
+    }
+
+    public void onButton11Click(View v) {
+        mpG.seekTo(0);
+        Log.i(TAG, "Button 11 Clicked");
+        mpG.start();
+
+    }
+
+    public void onButton12Click(View v) {
+        mpGS.seekTo(0);
+        Log.i(TAG, "Button 12 Clicked");
+        mpGS.start();
+
+    }
+
+    public void onButton13Click(View v) {
+        mpHighE.seekTo(0);
+        Log.i(TAG, "Button 13 Clicked");
+        mpHighE.start();
+
+    }
+
+    public void onButton14Click(View v) {
+        mpHighF.seekTo(0);
+        Log.i(TAG, "Button 14 Clicked");
+        mpHighF.start();
+
+    }
+
+    public void onButton15Click(View v) {
+        mpHighFS.seekTo(0);
+        Log.i(TAG, "Button 15 Clicked");
+        mpHighFS.start();
+
+    }
+
+    public void onButton16Click(View v) {
+        mpHighG.seekTo(0);
+        Log.i(TAG, "Button 16 Clicked");
+        mpHighG.start();
+
+    }
+
+
+    @Override
+    public void onCompletion(MediaPlayer mp) {
+
+    }
 }
-
-
-
-
-
-
